c7a0b4ec5590ae6f5aef4c3ae9688b50adf757fa
Nguyễn Trọng Nhân <62286534+TrongNhan-Nguyen@users.noreply.github.com>
2020-08-16 16:34:58 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-
+// Test pull
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
