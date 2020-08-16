c43ffcf91f4a6719acee3778cf2ddaad61b6983f
Trong Nhan <trongnhannguyen94@gmail.com>
2020-08-16 16:07:44 +0700

    Hello Come Home

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello React native 3</Text>
+    </View>
+  );
+};
+
+export default App;
